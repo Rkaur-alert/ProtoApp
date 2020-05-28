1baf2a433497db92d2a019d5cf633a29723cda5c
Rashmeet Kaur <rashmeet.kaur@alert-ab.ca>
2020-05-28 10:47:21 -0600

    Working before validation

---
+++ ProtoApp/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
+
+namespace ProtoApp.Models
+{
+    public class Min18YearsIfAMember: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(Object value, ValidationContext validationContext)
+        {
+            return base.IsValid(value,validationContext);
+        }
+    }
+}
