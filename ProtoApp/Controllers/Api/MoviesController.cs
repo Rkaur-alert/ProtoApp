dff3a31e76dcee9d0eec278b9ff17b6bbb185520
Rashmeet Kaur <rashmeet.kaur@alert-ab.ca>
2020-06-09 14:26:29 -0600

    Api movies Get movie working

---
+++ ProtoApp/Controllers/Api/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using ProtoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using ProtoApp.Dtos;
+
+namespace ProtoApp.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET /api/movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        //GET /api/movies/1
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return movie;
+        }
+
+        //POST /api/movies
+    }
+}
